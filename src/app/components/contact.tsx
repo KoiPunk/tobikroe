e0b3e62a9477620909558f6d57f946bdba496861
KoiPunk <100730947+KoiPunk@users.noreply.github.com>
2026-01-30 22:11:29 -0800

    redeploy

--- src/app/components/contact.tsx
+++ src/app/components/contact.tsx
@@ -8,24 +8,24 @@ export default function Contact() {
         <p>* INDICATES REQUIRED FIELD</p>
         <div className="flex">
             <section>
-                <label for="name">Name *</label>
+                <label htmlFor="name">Name *</label>
                 <input id="name" type="text" name="name" required></input>
             </section>
 
             <section>
-                <label for="email">Email *</label>
+                <label htmlFor="email">Email *</label>
                 <input id="email" type="text" name="email" required></input>
             </section>
         </div>
 
         <section>
-            <label for="subject">Subject *</label>
+            <label htmlFor="subject">Subject *</label>
             <input id="subject" type="text" name="subject" required></input>
         </section>
 
         <section>
-            <label for="message">Word *</label>
-            <textarea id="message" name="message" rows="5" cols="" required></textarea>
+            <label htmlFor="message">Word *</label>
+            {/* <textarea id="message" name="message" rows="5" cols="" required></textarea> */}
         </section>
 
         <input type="submit" value="Submit" className="bg-black text-white w-fit px-8 py-2"/>
